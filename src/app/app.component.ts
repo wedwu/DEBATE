56fc69b73aa1b7b752e87d8ebea9335d57a2a833
Christopher Watson <chris@watsonink.com>
2024-06-27 15:21:29 -0400

    WIP: bugs in the regex

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -59,12 +59,17 @@ export class AppComponent implements OnInit {
 
   public cardRandom: any[any] = []
   public selected: any
+  public selectedIndexNumber: number = 12
+  public selectedIndex: [any] = [12]
+  public FREESQUARE: string = 'FREE SQUARE'
+  public BINGO: string = ''
 
   constructor() {}
 
   ngOnInit() {
     this.cardRandom = this.getRandomUniqueItems(this.cardDefaults, 25)
     localStorage.setItem('selected', JSON.stringify(this.cardRandom))
+    localStorage.setItem('selectedIndex', JSON.stringify(this.selectedIndex))
   }
 
   getRandomUniqueItems = (arr: any[], count: any = 25) => {
@@ -76,8 +81,8 @@ export class AppComponent implements OnInit {
       const use = item.word
       if (!used[use]) {
         if (result.length === 12) {
-          result.push({word: 'FREE SQUARE', selected: true})
-          used['FREE SQUARE'] = true
+          result.push({word: this.FREESQUARE, selected: true})
+          used[this.FREESQUARE] = true
         } else {
           result.push(item)
           used[use] = true
@@ -88,7 +93,8 @@ export class AppComponent implements OnInit {
     return result;
   }
 
-  onSelect = (n: any) => {
+  onSelect = (n: any, i: number) => {
+    if (n.word === this.FREESQUARE) return
     this.cardRandom = this.cardRandom.map((word: any) => {
       if (word.word === n.word) {
        word.selected = !!!word.selected       
@@ -97,7 +103,74 @@ export class AppComponent implements OnInit {
       return word
     })
     localStorage.setItem('selected', JSON.stringify(this.cardRandom))
-    console.log(this.cardRandom)
+    this.showBingo(i)
+  }
+
+  showBingo = (i: number) => {
+    // ... make sure array doesn't have the index already and needs to be sorted
+    let res: any = localStorage.getItem('selectedIndex')
+    res = JSON.parse(res)
+    this.popIfExists(res, i)
+  }
+
+  popIfExists = (arr: any, number: number) => {
+    const index = arr.indexOf(number)
+    if (index !== -1) {
+      arr.splice(index, 1)
+    } else {
+      arr.push(number)   
+    }
+    const sortedValues = arr.toSorted((a: number, b: number) => a - b)
+    localStorage.setItem('selectedIndex', JSON.stringify(sortedValues))
+    this.BINGO = this.containsBingo(sortedValues) ? 'BINGO' : ''
+  }
+
+  containsBingo = (arr: any) => {
+    const arrString = arr.join('')
+    const regex1: any = /(?=.*0)(?=.*1)(?=.*2)(?=.*3)(?=.*4)/
+    const regex2: any = /(?=.*5)(?=.*6)(?=.*7)(?=.*8)(?=.*9)/
+    const regex3: any = /(?=.*10)(?=.*11)(?=.*12)(?=.*13)(?=.*14)/
+    const regex4: any = /(?=.*15)(?=.*16)(?=.*17)(?=.*18)(?=.*19)/
+    const regex5: any = /(?=.*20)(?=.*21)(?=.*22)(?=.*23)(?=.*24)/
+    const regex6: any = /(?=.*0)(?=.*5)(?=.*10)(?=.*15)(?=.*20)/
+    const regex7: any = /(?=.*1)(?=.*6)(?=.*11)(?=.*16)(?=.*21)/
+    const regex8: any = /(?=.*2)(?=.*7)(?=.*12)(?=.*17)(?=.*22)/
+    const regex9: any = /(?=.*3)(?=.*8)(?=.*13)(?=.*18)(?=.*23)/
+    const regex10: any = /(?=.*4)(?=.*9)(?=.*14)(?=.*19)(?=.*24)/ 
+    const regex11: any = /(?=.*0)(?=.*6)(?=.*12)(?=.*18)(?=.*24)/ 
+    const regex12: any = /(?=.*4)(?=.*8)(?=.*12)(?=.*16)(?=.*20)/ 
+
+    if (regex1.test(arrString)) {
+      return true
+    } else if (regex2.test(arrString)) {
+      return true
+    } else if (regex3.test(arrString)) {
+      return true
+    } else if (regex4.test(arrString)) {
+      return true
+    } else if (regex5.test(arrString)) {
+      return true
+    } else if (regex6.test(arrString)) {
+      if (arrString === '10121520' || arrString === '510121520')return false
+      return true
+    } else if (regex7.test(arrString)) {
+      if (arrString === '11121621' || arrString === '611121621')return false
+      return true
+    } else if (regex8.test(arrString)) {
+      if (arrString === '121722' || arrString === '7121722')return false
+      return true
+    } else if (regex9.test(arrString)) {
+      if (arrString === '12131823' || arrString === '812131823')return false
+      return true
+    } else if (regex10.test(arrString)) {
+      if (arrString === '12141924' || arrString === '912141924')return false
+      return true
+    } else if (regex11.test(arrString)) {
+      return true
+    } else if (regex12.test(arrString)) {
+      return true
+    }
+    return false
   }
 
   onClick = () => {
